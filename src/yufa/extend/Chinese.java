ac4c8e9c191e8b4c9e8da3b36cb0e2f29fe01e40
liangyong <liangyong@rrkd.cn>
2017-06-16 14:08:33 +0800

    继承 基础语法

---
+++ src/yufa/extend/Chinese.java
@@ -0,0 +1,12 @@
+package yufa.extend;
+
+/**
+ * 继承
+ * Created by alibeibei on 2017/6/16.
+ */
+public class Chinese extends People {
+    public static void main(String[] args) {
+        Chinese chinese = new Chinese();
+        chinese.name();
+    }
+}
---
+++ src/yufa/extend/People.java
@@ -0,0 +1,10 @@
+package yufa.extend;
+
+/**
+ * Created by alibeibei on 2017/6/16.
+ */
+public class People {
+    public void name() {
+        System.out.print("my is a People");
+    }
+}
