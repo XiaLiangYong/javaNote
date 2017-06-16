791b636be8a1d05739afffe18b262713360be09d
liangyong <liangyong@rrkd.cn>
2017-06-16 14:04:04 +0800

    第一个java程序

---
+++ src/yufa/test.java
@@ -0,0 +1,10 @@
+package yufa;
+
+/**
+ * Created by alibeibei on 2017/6/16.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
+}
