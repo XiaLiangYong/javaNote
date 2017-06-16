2e1e571e89d0db11d54a5df3398670a4d42956b5
liangyong <liangyong@rrkd.cn>
2017-06-16 13:36:40 +0800

    第一个java程序

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+/**
+ * Created by alibeibei on 2017/6/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
