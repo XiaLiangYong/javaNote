0e9680f4f78ee3d9b518f092f5c028548dbb7352
liangyong <liangyong@rrkd.cn>
2017-06-27 15:51:23 +0800

    常用类测试

---
+++ src/yufa/testClass/testStringBuffer.java
@@ -0,0 +1,34 @@
+package yufa.testClass;
+
+/**
+ * Created by alibeibei on 2017/6/27.
+ */
+public class testStringBuffer {
+    public static void main(String[] args) {
+        StringBuffer sBuffer = new StringBuffer("菜鸟教程官网：");
+        sBuffer.append("www");
+        sBuffer.append(".runoob");
+        sBuffer.append(".com");
+        System.out.println(sBuffer);
+        // 数组大小
+        int size = 10;
+        // 定义数组
+        double[] myList = new double[size];
+        myList[0] = 5.6;
+        myList[1] = 4.5;
+        myList[2] = 3.3;
+        myList[3] = 13.2;
+        myList[4] = 4.0;
+        myList[5] = 34.33;
+        myList[6] = 34.0;
+        myList[7] = 45.45;
+        myList[8] = 99.993;
+        myList[9] = 11123;
+        // 计算所有元素的总和
+        double total = 0;
+        for (int i = 0; i < size; i++) {
+            total += myList[i];
+        }
+        System.out.println("总和为： " + total);
+    }
+}
